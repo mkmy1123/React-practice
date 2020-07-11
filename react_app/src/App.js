a8a2c32aa244b615ff9ca6bceb6628968b2e9d17
Maki Miyagawa <61111655+mkmy1123@users.noreply.github.com>
2020-07-11 11:39:08 +0900

    ADD state : if 三項

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -4,16 +4,24 @@ import './App.css';
 
 class App extends Component {
 
-  msgStyle = {
+  msgStyle1 = {
     fontSize: "24pt",
     color:"#900",
     margin: "20px 0px",
     padding: "5px",
     borderBottom: "2px solid #900"
   }
-  btnStyle = {
+  msgStyle2 = {
     fontSize:"20pt",
-    padding:"0px 10px"
+    color:"white",
+    backgroundColor:"#900",
+    margin: "20px 0px",
+    padding:"0px 10px",
+    borderBottom:"2px solid #900"
+  }
+  btnStyle = {
+    fontSize:"16px",
+    padding:"10px"
   }
 
   constructor(props){
@@ -21,6 +29,7 @@ class App extends Component {
     this.state = {
       counter: 0,
       msg: 'count start!',
+      flg:true,
     };
     this.doAction = this.doAction.bind(this);
   }
@@ -28,14 +37,19 @@ class App extends Component {
   doAction(e){
     this.setState((state)=>({
       counter: state.counter + 1,
-      msg: 'count: ' + state.counter
+      msg: 'count: ' + state.counter,
+      flg: !state.flg
     }));
   }
 
   render(){
     return <div>
       <h1>React</h1>
-      <p style={this.msgStyle}>{this.state.msg}</p>
+      {this.state.flg ?
+        <p style={this.msgStyle1}>{this.state.msg}</p>
+      :
+        <p style={this.msgStyle2}>{this.state.msg}です。</p>
+      }
       <button style={this.btnStyle} onClick={this.doAction}>Click</button>
     </div>;
   }
