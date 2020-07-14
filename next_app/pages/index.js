b6fec50b34b5041ec034a07078620e905a9a48a0
Maki Miyagawa <61111655+mkmy1123@users.noreply.github.com>
2020-07-14 18:28:06 +0900

    ページ遷移

--- next_app/pages/index.js
+++ next_app/pages/index.js
@@ -1,20 +1,15 @@
-export default () =><div>
-  <style jsx>{`
-  h1 {
-    font-size:72pt;
-    font-weight:bold;
-    text-align:right;
-    letter-spacing:-8px;
-    color:#f0f0f0;
-    margin:-40px 0px;
-  }
-  p {
-    margin:0px;
-    color:#666;
-    fontSize:16pt;
-  }
-  `}</style>
+import Link from 'next/link';
+import style from '../static/Style';
+
+export default () => <div>
+  {style}
   <h1>Next.js</h1>
   <p>Welcome to next.js!</p>
-  <p>これはビルトインCSSによるスタイルです。</p>
+  <hr />
+  <div>
+    <Link href="/other">
+      <button>Go to Other page &gt;&gt;</button>
+    </Link>
+  </div>
 </div>
+
---
+++ next_app/pages/other.js
@@ -0,0 +1,27 @@
+import Link from 'next/link';
+
+const h1 = {
+  fontSize: '72pt',
+  fontWeight: 'bold',
+  textAlign: 'right',
+  letterSpacing: '-8px',
+  color: '#f0f0f0',
+  margin: '-40px 0px',
+}
+const p = {
+  margin: '0px',
+  color: '#666',
+  fontSize: '16pt',
+}
+
+export default () => <div>
+  <h1>Next.js</h1>
+  <p>Welcome to next.js!</p>
+  <hr />
+  <div>
+    <Link href="/">
+      <a>&lt;&lt; Back to Index page</a>
+    </Link>
+  </div>
+</div>
+
