78665d74563869ea48a15c9194b946c907e1d6d0
Maki Miyagawa <61111655+mkmy1123@users.noreply.github.com>
2020-07-14 17:33:14 +0900

    ビルトインCSS

--- next_app/pages/index.js
+++ next_app/pages/index.js
@@ -1,4 +1,20 @@
 export default () =><div>
+  <style jsx>{`
+  h1 {
+    font-size:72pt;
+    font-weight:bold;
+    text-align:right;
+    letter-spacing:-8px;
+    color:#f0f0f0;
+    margin:-40px 0px;
+  }
+  p {
+    margin:0px;
+    color:#666;
+    fontSize:16pt;
+  }
+  `}</style>
   <h1>Next.js</h1>
-  <div>Welcome to next.js!</div>
+  <p>Welcome to next.js!</p>
+  <p>これはビルトインCSSによるスタイルです。</p>
 </div>
