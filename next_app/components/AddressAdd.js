3ee54ef4c2f1f84ab68215f97f24b6c6e1cc9e2f
Maki Miyagawa <61111655+mkmy1123@users.noreply.github.com>
2020-07-16 01:59:53 +0900

    本当に完成

--- next_app/components/AddressAdd.js
+++ next_app/components/AddressAdd.js
@@ -1,28 +1,28 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import Router from 'next/router';
+import Router from "next/router";
 import firebase from "firebase";
 import Lib from "../static/address_lib";
 import Account from "../components/Account";
 
 class AddressAdd extends Component {
   style = {
-    fontSize:"12pt",
-    padding:"5px 10px"
-  }
+    fontSize: "12pt",
+    padding: "5px 10px",
+  };
 
-  constructor(props){
+  constructor(props) {
     super(props);
-    if (this.props.login == false){
-      Router.push('/address');
+    if (this.props.login == false) {
+      Router.push("/address");
     }
     this.state = {
-      name:'',
-      email:'',
-      tel:'',
-      memo:'',
-      message:'データを入力してください。'
-    }
+      name: "",
+      email: "",
+      tel: "",
+      memo: "",
+      message: "データを入力してください。",
+    };
     this.logined = this.logined.bind(this);
     this.onChangeName = this.onChangeName.bind(this);
     this.onChangeEmail = this.onChangeEmail.bind(this);
@@ -31,86 +31,123 @@ class AddressAdd extends Component {
     this.doAction = this.doAction.bind(this);
   }
 
-  logined(){
-    console.log('logined.');
+  logined() {
+    console.log("logined.");
   }
-  logouted(){
-    Router.push('/address');
+  logouted() {
+    Router.push("/address");
   }
 
-  onChangeName(e){
-    this.setState({name:e.target.value});
+  onChangeName(e) {
+    this.setState({ name: e.target.value });
   }
-  onChangeEmail(e){
+  onChangeEmail(e) {
     this.setState({ email: e.target.value });
   }
-  onChangeTel(e){
+  onChangeTel(e) {
     this.setState({ tel: e.target.value });
   }
-  onChangeMemo(e){
+  onChangeMemo(e) {
     this.setState({ memo: e.target.value });
   }
 
-  doAction(e){
-    debugger;
+  doAction(e) {
     let key = this.state.email;
     let data = {
-      name:this.state.name,
-      tel:this.state.tel,
-      memo:this.state.memo
-    }
+      name: this.state.name,
+      tel: this.state.tel,
+      memo: this.state.memo,
+    };
     let db = firebase.database();
-    let ref = db.ref('address/' + Lib.encodeEmail(this.props.email) + '/' + Lib.encodeEmail(this.state.email));
+    let ref = db.ref(
+      "address/" +
+        Lib.encodeEmail(this.props.email) +
+        "/" +
+        Lib.encodeEmail(this.state.email)
+    );
     console.log(ref);
     ref.set(data);
     this.setState({
-      name:'',
-      email:'',
-      tel:'',
-      memo:'',
-      message:'※登録しました。'
-    })
+      name: "",
+      email: "",
+      tel: "",
+      memo: "",
+      message: "※登録しました。",
+    });
   }
 
-  render(){
+  render() {
     return (
-    <div>
-      <Account self={this} onLogined={this.logined} onLogouted={this.logouted} />
-      <hr />
-      <p>{this.state.message}</p>
-      {this.props.login
-      ?
-      <table>
-        <tbody>
-          <tr>
-            <th>name:</th>
-            <td><input type="text" size="30" value={this.state.name} onChange={this.onChangeName} /></td>
-          </tr>
-          <tr>
-            <th>email:</th>
-            <td><input type="text" size="30" value={this.state.email} onChange={this.onChangeEmail} /></td>
-          </tr>
-          <tr>
-            <th>tel:</th>
-            <td><input type="text" size="30" value={this.state.tel} onChange={this.onChangeTel} /></td>
-          </tr>
-          <tr>
-            <th>memo:</th>
-            <td><input type="text" size="30" value={this.state.memo} onChange={this.onChangeMemo} /></td>
-          </tr>
-          <tr>
-            <th></th>
-            <td><button onClick={this.doAction}>Add</button></td>
-          </tr>
-        </tbody>
-      </table>
-      :
-      <p>please login ...</p>
-      }
-    </div>
+      <div>
+        <Account
+          self={this}
+          onLogined={this.logined}
+          onLogouted={this.logouted}
+        />
+        <hr />
+        <p>{this.state.message}</p>
+        {this.props.login ? (
+          <table>
+            <tbody>
+              <tr>
+                <th>name:</th>
+                <td>
+                  <input
+                    type="text"
+                    size="30"
+                    value={this.state.name}
+                    onChange={this.onChangeName}
+                  />
+                </td>
+              </tr>
+              <tr>
+                <th>email:</th>
+                <td>
+                  <input
+                    type="text"
+                    size="30"
+                    value={this.state.email}
+                    onChange={this.onChangeEmail}
+                  />
+                </td>
+              </tr>
+              <tr>
+                <th>tel:</th>
+                <td>
+                  <input
+                    type="text"
+                    size="30"
+                    value={this.state.tel}
+                    onChange={this.onChangeTel}
+                  />
+                </td>
+              </tr>
+              <tr>
+                <th>memo:</th>
+                <td>
+                  <input
+                    type="text"
+                    size="30"
+                    value={this.state.memo}
+                    onChange={this.onChangeMemo}
+                  />
+                </td>
+              </tr>
+              <tr>
+                <th></th>
+                <td>
+                  <button onClick={this.doAction}>Add</button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        ) : (
+          <p>please login ...</p>
+        )}
+      </div>
     );
   }
 }
 
-AddressAdd = connect((state)=>state)(AddressAdd);
+AddressAdd = connect((state) => state)(AddressAdd);
 export default AddressAdd;
